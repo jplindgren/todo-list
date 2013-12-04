1a1ea5aa690a7ff4283cb4fa30453a244e5f41f5
jplindgren <jplindgrenbr@hotmail.com>
2013-12-03 23:01:16 -0200

    blahblah

--- concepts/closureTests.js
+++ concepts/closureTests.js
@@ -40,3 +40,20 @@ function warningMaker( obstacle ){
          );
   };
 }
+
+
+function makeTargetAssigner( sharks, targets ){
+  return function(shark){
+    for (var i =0; i < sharks.length; i++){
+      if (shark == sharks[i]){
+        alert("What up, " + shark + "\n" +
+              "There've been " +
+              targets[i] + " sightings in our `hood!\n" +
+              "Time for a swim-by lasering, homie!"
+             );
+      }
+    }
+  };
+}
+
+
